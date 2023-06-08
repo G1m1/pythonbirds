aa925dc4f9e140ad3ec016ff3c6f35bcad977eac
Gamaliel <marronzinho.mg@hotmail.com>
2023-06-08 16:14:49 -0300

    Criado atribudo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,13 @@
 class Pessoa():
+    olhos = 2 # atributo de classe | significa que o valor vai único para todos os objetos. Otimiza seu tempo.
+
     def __init__(self, * filhos, nome=None,idade=35): # atributo dado| instância da classe.
         self.nome = nome
         self.idade = idade
         self.filhos = list(filhos)  # *filhos = recebe uma quantidade variavel de parâmetros | estou criando uma lista, se liga, lista aceita qualquer tipo de variavel.
 
     def cumprimentar(self): # método
-        return 'ola'
+        return 'Ola, bom dia !'
 
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo') # pai do luciano
@@ -25,11 +27,17 @@ if __name__ == '__main__':
     print(luciano.__dict__)                   # o atributo especial dict te fala todos os atributos de um objeto do tipo classe
     print(renzo.__dict__)                       # luciano e renzo são diferentes.
     print()
-    print()
     del luciano.filhos # não é muito usual, mas é possivel excluir um atributo, apenas para conhecimento
+    luciano.olhos = 1 # vc pode mudar o atributo de classe so para um objeto, sem alterar os demais.
     print()
     print()
+    print(luciano.olhos)
+    print(Pessoa.olhos) # olha que legal, por ser um atributo da classe, ele também consegue ser acessado via Classe.
     print()
+    print()
+    print(f'{luciano.cumprimentar()} sou o {luciano.nome} tenho {luciano.idade} anos de idade.')
+    print()
+    print(luciano.olhos,Pessoa.olhos) # o objeto mudou a quantidade de olhos, mas a classe não foi alterada o  seu valor.
 
 
 
