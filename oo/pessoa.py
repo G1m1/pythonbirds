04305ea2b3c01a931909ff7098ff1f6a4546be64
Gamaliel <marronzinho.mg@hotmail.com>
2023-06-08 11:55:18 -0300

    'testando'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,20 +1,11 @@
+'testando'
+
 class Pessoa:
-    def __init__(self,nome=None,idade=32):
-        self.nome = nome
-        self.idade = idade
-
-    def cumprimentar(self):
-        return f'Olá  {id(self)}'
+    pass
 
 
-if __name__ == '__main__':
-    p = Pessoa("Gamaliel")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Renzo"
-    print(p.nome)
-    print(p.idade)
+
+
+
 
 
