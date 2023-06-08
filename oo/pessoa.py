ed6bda1e2c12a20054d0fb2b2a2a73996d396092
Gamaliel <marronzinho.mg@hotmail.com>
2023-06-08 15:22:33 -0300

    Criado atributos de instâncias nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,21 @@
 class Pessoa():
+    def __init__(self,nome = None,idade = 35,cidade = 'Cataguases'):
+        self.nome = nome
+        self.idade = idade
+
+
+
     def cumprimentar(self):
         return 'ola'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Gama')
+
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "Gamaliel"
+    print(p.nome)
+    print(p.idade)
+
+
+
