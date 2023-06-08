614246d0b75c65707faad51bdd1aa612fcf458c9
Gamaliel <marronzinho.mg@hotmail.com>
2023-06-08 15:45:12 -0300

    Criado atributo compleso filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,26 @@
 class Pessoa():
-    def __init__(self,nome = None,idade = 35,cidade = 'Cataguases'):
+    def __init__(self, * filhos, nome=None,idade=35): # atributo dado| instância da classe.
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)  # *filhos = recebe uma quantidade variavel de parâmetros | estou criando uma lista, se liga, lista aceita qualquer tipo de variavel.
 
-
-
-    def cumprimentar(self):
+    def cumprimentar(self): # método
         return 'ola'
 
 if __name__ == '__main__':
-    p = Pessoa('Gama')
+    renzo = Pessoa(nome='Renzo') # pai do luciano
+    luciano = Pessoa(renzo, nome='Luciano') # filho do renzo
 
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Gamaliel"
-    print(p.nome)
-    print(p.idade)
+    print(luciano.cumprimentar())
+    print()
+    print(luciano.nome)
+    print(luciano.idade)
+    print()
+    print()
+    for filho in luciano.filhos:
+        print(filho.nome)
+
+
 
 
 
